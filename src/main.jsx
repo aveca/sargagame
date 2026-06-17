6f605596d736f8a19a8d8f1b92b75ffa640f6d53
aveca <yacovassaraf@gmail.com>
2026-06-17 06:26:28 -0400

    refactor: move root JSX components to src directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../Sargasses_PROD.jsx'
+import App from './Sargasses_PROD.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
